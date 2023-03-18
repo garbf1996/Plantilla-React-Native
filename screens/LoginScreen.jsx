c91ddf6c7b48e9ebb4a3cd85da0e77f8a6132f1b
garber <garbfbatista@gmail.com>
2023-03-18 11:32:05 -0400

    Login design complete

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "react-native";
 
 export const LoginScreen = () => {
   return (
     <View className='bg-white h-screen flex items-center'>
       <StatusBar />
-      <View className='bg-yellow-400 w-11/12 h-3/6 rounded-3xl mt-4'></View>
+      <View className='bg-sky-600 w-11/12 h-3/6 rounded-3xl mt-4  flex justify-center items-center'>
+        <Text className='text-white font-bold text-3xl'>Logo</Text>
+      </View>
 
-      <Text className='text-black font-extrabold text-2xl'>
+      <Text className='text-black font-extrabold text-4xl mt-7 text-center'>
         Run your business better
       </Text>
-      <Text>
+      <Text className='w-80 text-center tracking-wide mt-5'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ipsum ad
         ratione molestias sint id! Odio impedit eum, atque molestias quidem
-        perspiciatis reprehenderit magnam, quasi aliquam dolorum voluptatibus
-        adipisci suscipit.
+        perspiciatis reprehenderit.
       </Text>
 
-      <TouchableOpacity>
-        <Text>Sign in</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text>Register</Text>
-      </TouchableOpacity>
+      <View className='flex flex-row justify-center w-5/6 h-20 mt-8'>
+        <TouchableOpacity className='flex justify-center items-center bg-sky-600 w-44 rounded-tl-2xl rounded-bl-2xl shadow-lg shadow-gray-800'>
+          <Text className='text-white font-bold text-lg'>Sign in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className='flex justify-center items-center bg-gray-200 w-32 rounded-tr-2xl rounded-br-2xl shadow-lg shadow-gray-800'>
+          <Text className='font-bold text-lg'>Register</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
