2d7b89a5ca5351e7e8fa4b583e3f4a55d2a78fe8
garber <garbfbatista@gmail.com>
2023-03-20 16:07:59 -0400

    Cambio de paleta de colores

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -4,27 +4,27 @@ import { StatusBar } from "react-native";
 
 export const LoginScreen = () => {
   return (
-    <View className='bg-white h-screen flex items-center'>
+    <View className='bg-black h-screen flex items-center'>
       <StatusBar />
-      <View className='bg-sky-600 w-11/12 h-3/6 rounded-3xl mt-4  flex justify-center items-center'>
+      <View className='bg-[#1C1C1C] w-11/12 h-3/6 rounded-3xl mt-4  flex justify-center items-center'>
         <Text className='text-white font-bold text-3xl'>Logo</Text>
       </View>
 
-      <Text className='text-black font-extrabold text-4xl mt-7 text-center'>
+      <Text className='text-[#EDEDED] font-extrabold text-4xl mt-7 text-center'>
         Run your business better
       </Text>
-      <Text className='w-80 text-center tracking-wide mt-5'>
+      <Text className='w-80 text-[#A0A0A0] text-center tracking-wide mt-5'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ipsum ad
         ratione molestias sint id! Odio impedit eum, atque molestias quidem
         perspiciatis reprehenderit.
       </Text>
 
       <View className='flex flex-row justify-center w-5/6 h-20 mt-8'>
-        <TouchableOpacity className='flex justify-center items-center bg-sky-600 w-44 rounded-tl-2xl rounded-bl-2xl shadow-lg shadow-gray-800'>
-          <Text className='text-white font-bold text-lg'>Sign in</Text>
+        <TouchableOpacity className='flex justify-center items-center bg-[#2E2E2E] w-44 rounded-tl-2xl rounded-bl-2xl '>
+          <Text className='text-[#EDEDED] font-bold text-lg'>Sign in</Text>
         </TouchableOpacity>
-        <TouchableOpacity className='flex justify-center items-center bg-gray-200 w-32 rounded-tr-2xl rounded-br-2xl shadow-lg shadow-gray-800'>
-          <Text className='font-bold text-lg'>Register</Text>
+        <TouchableOpacity className='flex justify-center items-center bg-[#F3F3F3] w-32 rounded-tr-2xl rounded-br-2xl '>
+          <Text className='font-bold text-lg text-[#232323]'>Register</Text>
         </TouchableOpacity>
       </View>
     </View>
