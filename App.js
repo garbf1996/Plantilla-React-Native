ba043328220fcc6b4bffb8012d03f05774f36bbf
garber <garbfbatista@gmail.com>
2023-03-21 16:34:24 -0400

    Navigacion login en proceso

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { Welcome } from "./screens/Welcome";
 import { LoginScreen } from "./screens/LoginScreen";
 
+const { Navigator, Screen } = createStackNavigator();
+
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Screen name='Home' component={Welcome} />
+        <Screen name='Login' component={LoginScreen} />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,31 +1,24 @@
-import React from "react";
-import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
-import { StatusBar } from "react-native";
+import { Text, View, TextInput } from "react-native";
 
 export const LoginScreen = () => {
   return (
-    <View className='bg-black h-screen flex items-center'>
-      <StatusBar />
-      <View className='bg-[#1C1C1C] w-11/12 h-3/6 rounded-3xl mt-4  flex justify-center items-center'>
-        <Text className='text-white font-bold text-3xl'>Logo</Text>
-      </View>
-
-      <Text className='text-[#EDEDED] font-extrabold text-4xl mt-7 text-center'>
-        Run your business better
-      </Text>
-      <Text className='w-80 text-[#A0A0A0] text-center tracking-wide mt-5'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ipsum ad
-        ratione molestias sint id! Odio impedit eum, atque molestias quidem
-        perspiciatis reprehenderit.
+    <View>
+      <Text>Welcome Back</Text>
+      <Text>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+        doloremque illo itaque explicabo nam, voluptatem aspernatur eius
+        molestiae
       </Text>
 
-      <View className='flex flex-row justify-center w-5/6 h-20 mt-8'>
-        <TouchableOpacity className='flex justify-center items-center bg-[#2E2E2E] w-44 rounded-tl-2xl rounded-bl-2xl '>
-          <Text className='text-[#EDEDED] font-bold text-lg'>Sign in</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className='flex justify-center items-center bg-[#F3F3F3] w-32 rounded-tr-2xl rounded-br-2xl '>
-          <Text className='font-bold text-lg text-[#232323]'>Register</Text>
-        </TouchableOpacity>
+      <View>
+        <TextInput
+          keyboardType='visible-password'
+          className='w-full h-14 border border-slate-800'
+        />
+        <TextInput
+          className=' w-full h-14 bg-slate-800'
+          placeholderTextColor='white'
+        />
       </View>
     </View>
   );
